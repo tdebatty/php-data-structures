5a496b5c9b656ceeabea1bd5e72633ac77b14698
Thibault Debatty <thibault.debatty@gmail.com>
2013-08-21 12:05:52 +0200

    tests

--- src/webd/structures/KDTree.php
+++ src/webd/structures/KDTree.php
@@ -7,54 +7,69 @@ class KDTree
      *
      * @var \webd\vectors\Vector 
      */
-    public $point;
+    protected $point;
     
     /**
      *
      * @var KDTree 
      */
-    public $left;
+    protected $left;
     
     /**
      *
      * @var KDTree
      */
-    public $right;
+    protected $right;
     
     
     
     public function __construct(array $points, $depth = 0) {
+        /* @var $points [\webd\vectors\Vector] */
         if (empty($points)) {
             return ;
         }
         
-        $dimension = $points[0]->dim();
+        $dimension = $points[0]->length();
         
         // Axis that will be used for splitting
         $axis = $depth % $dimension;
         
-        echo "Sort on $axis... ";
-        $sort_function = sort_factory($axis);
         // Sort points on choosen axis
+        $sort_function = self::sort_factory($axis);
         usort($points, $sort_function);
-        echo "done\n";
         
         // Choose median point (on choosen axis)
         $median = floor(count($points) / 2);
-        
         $this->point = $points[$median];
         
         $this->left = new KDTree(array_slice($points, 0, $median -1), $depth + 1);
         $this->right = new KDTree(array_slice($points, $median +1), $depth + 1);
     }
+    
+    public function getPoint() {
+        return $this->point;
+    }
+    
+    /**
+     * 
+     * @return KDTree
+     */
+    public function getLeft() {
+        return $this->left;
+    }
+    
+    public function getRight() {
+        return $this->right;
+    }
+    
+    protected static function sort_factory($axis) {
+        return function ($a, $b) use ($axis) {
+            $a_value = $a->getValue();
+            $b_value = $b->getValue();
+            return ($a_value[$axis] < $b_value[$axis]) ? -1 : 1;
+        };
+    }
 }
 
-function sort_factory($axis) {
-    return function ($a, $b) use ($axis) {
-        $a_value = $a->getValue();
-        $b_value = $b->getValue();
-        return ($a_value[$axis] < $b_value[$axis]) ? -1 : 1;
-        //return 1;
-    };
-}
+
 ?>
